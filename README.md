50dc003c9c4c38f517ef7a4c416bdabd4c508a7a
Gustavo Gran <37720683+GustavoGran@users.noreply.github.com>
2023-08-20 15:10:25 +0000

    Added product description on `index.js`

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # clone-tabnews
 
 Implementação do tabnews para o curso.dev
+
+## Instalando dependências
+
+```
+npm install
+```
+
+## Rodando versão de testes locais
+
+```
+npm run devv
+```
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-  return <h1>Em construção! 👀</h1>;
+  return (
+    <>
+      <h1>
+        🚧 Uma nova forma de se preparar para resolver cases de processos
+        seletivos está em construção 🚧
+      </h1>
+      <ul>
+        <li>
+          Cases técnicos e um espaço único para discutir soluções de resolução
+        </li>
+        <li>Publique sua solução e acumule experiência!</li>
+        <li>Vote nas melhores soluções e também acumule experiência!</li>
+        <li>Suba no ranking com sua experiência acumulada</li>
+      </ul>
+    </>
+  );
 }
 
 export default Home;
